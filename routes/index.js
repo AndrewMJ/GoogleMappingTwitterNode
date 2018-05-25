a1ae5585f2051c35d91909c53edf2690a73478fb
arosslyn <36890385+arosslyn@users.noreply.github.com>
2018-05-25 17:12:30 -0400

    CRAB

---
+++ routes/index.js
@@ -0,0 +1,45 @@
+var express = require('express');
+var router = express.Router();
+
+var Twitter = require('twitter');
+var config = require('../config.js');
+
+var T = new Twitter(config);
+
+/* GET home page. */
+router.get('/askTwitter', function(req, res,) {
+
+  var params = {
+    q: '#homedepot, homedepot',
+    count: 5,
+    result_type: 'recent',
+    lang: 'en'
+  }
+  
+  T.get('search/tweets', params, (err, data, response) => {
+    // If there is no error, proceed
+    if(err){
+      return console.log(err);
+    }
+  
+    // Loop through the returned tweets
+    const parsedData = data.statuses
+         .map(tweet => (
+          { 
+            id: tweet.id_str,
+            location: tweet.user.location,
+            created_at: tweet.user.created_at,
+            profile_image_url: tweet.user.profile_image_url
+  
+  
+          })
+        );
+        console.log(parsedData);
+  
+    })
+
+  res.render('index', { title: 'Express' });
+});
+
+
+module.exports = router;
